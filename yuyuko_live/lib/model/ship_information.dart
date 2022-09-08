91dabb1a532193aa1bf70f0a26600558d43e0d63
Yiheng <development.henryquan@gmail.com>
2022-09-08 19:01:46 +1000

    try out freezed

--- yuyuko_live/lib/model/ship_information.dart
+++ yuyuko_live/lib/model/ship_information.dart
@@ -1,86 +1,47 @@
-import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-@immutable
-class ShipInformation {
-  const ShipInformation({
-    this.shipId,
-    this.shipInfo,
-    this.data,
-  });
+part 'ship_information.freezed.dart';
+part 'ship_information.g.dart';
 
-  final int? shipId;
-  final ShipInfo? shipInfo;
-  final ShipPRData? data;
+@freezed
+abstract class ShipInformation with _$ShipInformation {
+  const factory ShipInformation({
+    int? shipId,
+    ShipInfo? shipInfo,
+    ShipPRData? data,
+  }) = _ShipInformation;
 
   factory ShipInformation.fromJson(Map<String, dynamic> json) =>
-      ShipInformation(
-        shipId: json['shipId'] == null ? null : json['shipId'],
-        shipInfo: json['shipInfo'] == null
-            ? null
-            : ShipInfo.fromJson(json['shipInfo']),
-        data: json['data'] == null ? null : ShipPRData.fromJson(json['data']),
-      );
+      _$ShipInformationFromJson(json);
 }
 
-@immutable
-class ShipPRData {
-  const ShipPRData({
-    this.winRate,
-    this.averageDamageDealt,
-    this.averageFrags,
-  });
+@freezed
+abstract class ShipPRData with _$ShipPRData {
+  const factory ShipPRData({
+    double? winRate,
+    double? averageDamageDealt,
+    double? averageFrags,
+  }) = _ShipPRData;
 
-  final double? winRate;
-  final double? averageDamageDealt;
-  final double? averageFrags;
-
-  factory ShipPRData.fromJson(Map<String, dynamic> json) => ShipPRData(
-        winRate: json['winRate'] == null ? null : json['winRate'].toDouble(),
-        averageDamageDealt: json['averageDamageDealt'] == null
-            ? null
-            : json['averageDamageDealt'].toDouble(),
-        averageFrags: json['averageFrags'] == null
-            ? null
-            : json['averageFrags'].toDouble(),
-      );
+  factory ShipPRData.fromJson(Map<String, dynamic> json) =>
+      _$ShipPRDataFromJson(json);
 }
 
-@immutable
-class ShipInfo {
-  const ShipInfo({
-    this.nameCn,
-    this.nameEnglish,
-    this.level,
-    this.shipType,
-    this.country,
-    this.imgSmall,
-    this.imgLarge,
-    this.imgMedium,
-    this.shipIndex,
-    this.groupType,
-  });
+@freezed
+abstract class ShipInfo with _$ShipInfo {
+  const factory ShipInfo({
+    String? nameCn,
+    String? nameEnglish,
+    int? level,
+    String? shipType,
+    String? country,
+    String? imgSmall,
+    String? imgLarge,
+    String? imgMedium,
+    String? shipIndex,
+    String? groupType,
+  }) = _ShipInfo;
 
-  final String? nameCn;
-  final String? nameEnglish;
-  final int? level;
-  final String? shipType;
-  final String? country;
-  final String? imgSmall;
-  final String? imgLarge;
-  final String? imgMedium;
-  final String? shipIndex;
-  final String? groupType;
-
-  factory ShipInfo.fromJson(Map<String, dynamic> json) => ShipInfo(
-        nameCn: json['nameCn'],
-        nameEnglish: json['nameEnglish'],
-        level: json['level'],
-        shipType: json['shipType'],
-        country: json['country'],
-        imgSmall: json['imgSmall'],
-        imgLarge: json['imgLarge'],
-        imgMedium: json['imgMedium'],
-        shipIndex: json['shipIndex'],
-        groupType: json['groupType'],
-      );
+  factory ShipInfo.fromJson(Map<String, dynamic> json) =>
+      _$ShipInfoFromJson(json);
 }
