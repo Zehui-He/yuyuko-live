ec9dfd0a7eef00ec89a042e918c59b04d536afeb
Zehui-He <92138586+Zehui-He@users.noreply.github.com>
2022-09-21 15:31:59 +1000

    Refined wargaming_service

--- yuyuko_live/lib/model/player_result.dart
+++ yuyuko_live/lib/model/player_result.dart
@@ -5,9 +5,10 @@ part 'player_result.g.dart';
 
 @freezed
 abstract class PlayerResult with _$PlayerResult {
+  @JsonSerializable(fieldRename: FieldRename.snake)
   const factory PlayerResult({
     required String nickname,
-    @JsonKey(name: 'account_id') required int accountId,
+    required int accountId,
   }) = _PlayerResult;
 
   factory PlayerResult.fromJson(Map<String, dynamic> json) =>
